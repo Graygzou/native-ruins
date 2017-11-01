eb6bdd29f63c34a5c69044901a053dca54c5dba7
Graygzou <gregoire.boiron@gmail.com>
2017-11-01 01:09:50 -0400

    Wander encore moins wander

--- Assets/Scripts/OursAgentScript.cs
+++ Assets/Scripts/OursAgentScript.cs
@@ -14,21 +14,29 @@ public class OursAgentScript : MonoBehaviour {
     //public Transform target;
     NavMeshAgent agent;
 
+    private float m_dWanderRadius;
+
     // Use this for initialization
     void Start () {
         agent = GetComponent<NavMeshAgent>();
 
         //stuff for the wander behavior
         float theta = Random.value * 2 * Mathf.PI;
-        float m_dWanderRadius = 10f;
+        m_dWanderRadius = 3f;
         //create a vector to a target position on the wander circle
         m_vWanderTarget = new Vector3(m_dWanderRadius * Mathf.Cos(theta), 0f,
                                     m_dWanderRadius * Mathf.Sin(theta));
+        // Translation
+        m_vWanderTarget += transform.position + new Vector3(0,0,10);
 
+        //GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        //cube.transform.position = m_vWanderTarget;
+        //cube.GetComponent<Renderer>().material.color = new Color(0, 255, 52);
+        Debug.Log("first: " + transform.position + " et " + m_vWanderTarget);
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update () {
         //Debug.Log(gameObject.transform.forward);
         //print(transform.position.x);
         Wander();
@@ -36,18 +44,24 @@ public class OursAgentScript : MonoBehaviour {
 
     public void Wander()
     {
-        Vector3 test = new Vector3(Random.Range(-1.0f, 1.0f), 0f, Random.Range(-1.0f, 1.0f));
-        Debug.Log(test);
+        Vector3 test = new Vector3(Random.Range(-20.0f, 20.0f), 0f, Random.Range(-20.0f, 20.0f));
         Vector3 wanderTarget = m_vWanderTarget + test;
 
         //reproject this new vector back on to a unit circle
         wanderTarget.Normalize();
 
-        wanderTarget *= 3;
-        wanderTarget += agent.transform.forward * 6f;
-        //Debug.Log(target);
+        // Relative vector
+        Vector3 relativeVector = transform.position - m_vWanderTarget;
+        relativeVector.Normalize();
+        // Multiplie par le rayon du cercle wander
+        wanderTarget *= m_dWanderRadius;
+        // translation
+        wanderTarget += transform.position + relativeVector * 10f;
+        ////Debug.Log(target);
         m_vWanderTarget = wanderTarget;
 
+
+        Debug.Log(transform.position + " et " + m_vWanderTarget);
         agent.SetDestination(m_vWanderTarget);
 
         //cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
