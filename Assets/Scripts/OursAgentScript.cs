edc751c4d3079fb391359d22f40965d3f97023cb
Graygzou <gregoire.boiron@gmail.com>
2017-11-03 04:28:46 -0400

    Debut implementation MEF

--- Assets/Scripts/OursAgentScript.cs
+++ Assets/Scripts/OursAgentScript.cs
@@ -2,9 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using System;
 
-public class OursAgentScript : Animaux {
+public class BearScript : Animaux {
 
-    public OursAgentScript() : base(100, 5, 50) {
+    public float degat = 30.0f; // Can be tweak
+    public int nbRessourcesDropped = 2;
+
+    public BearScript() : base(80, 5, 50) {
     }
+
+    public void Update() {
+        // TODO
+    }
+
+    public void Attack() {
+        // TODO
+    }
+
+    public void Eat() {
+        // TODO
+    }
+
+    public void Death() {
+        // TODO
+    }
+
+}
+
+public abstract class Singleton {
+    private static Singleton instance;
+
+    private Singleton() { }
+
+    public static Singleton Instance {
+        get {
+            if (instance == null) {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+    }
+
+    public abstract void Enter(GameObject o);
+
+    public abstract void Execute(GameObject o);
+
+    public abstract void Exit(GameObject o);
+
 }
---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class State : MonoBehaviour {
+
+    //this will execute when the state is entered
+    public abstract void Enter(GameObject o);
+
+    //this is the states normal update function
+    public abstract void Execute(GameObject o);
+
+    //this will execute when the state is exited. 
+    public abstract void Exit(GameObject o);
+}
