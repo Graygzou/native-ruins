eaad3786d693ea722ad1604d6c4b46f9d43dd6a7
unknown <noemy.artigouha@gmail.com>
2017-11-04 23:45:24 -0400

    creation barres vie, faim et energie

---
+++ Assets/Scripts/Gauges/HungerBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HungerBar : MonoBehaviour {
+
+    public GameObject Hunger;
+    public Color BarColor;
+
+    //taille barre de vie est 150 mais on fait les calculs pour qu'elle soit sur 100
+
+	// Use this for initialization
+	void Start () {
+        Hunger.transform.Find("Mask").Find("Sprite").GetComponent<Image>().color = Color.green;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Hunger.GetComponent<Scrollbar>().size -= 0.001f;
+	}
+}
---
+++ Assets/Scripts/Gauges/LifeBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeBar : MonoBehaviour {
+
+    public GameObject Life;
+    public Color BarColor;
+
+    //taille barre de vie est 150 mais on fait les calculs pour qu'elle soit sur 100
+
+	// Use this for initialization
+	void Start () {
+        Life.transform.Find("Mask").Find("Sprite").GetComponent<Image>().color = Color.red;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Life.GetComponent<Scrollbar>().size -= 0.001f;
+	}
+}
