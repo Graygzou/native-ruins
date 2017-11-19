f1cc1b77569b50e699aaff32735d2d996e8f07ed
unknown <noemy.artigouha@gmail.com>
2017-11-19 16:34:26 -0500

    ajout des menus

--- Assets/Dialogues/DialogueTrigger.cs
+++ Assets/Dialogues/DialogueTrigger.cs
@@ -4,63 +4,58 @@ using UnityEngine;
 
 public class DialogueTrigger : MonoBehaviour {
 
-    public Dialogue dialogueDebut;
-    public Dialogue dialogueFin;
-    public Dialogue dialogueTotemOurs;
-    public Dialogue dialogueTotemPuma;
-    public Dialogue dialogueVoile;
-    public Dialogue dialogueCorde;
+    public Dialogue dialogue;
 
     public void TriggerDialogueDebut()
     {
-        dialogueDebut.name = "Judy";
-        dialogueDebut.sentences = new string[4];
-        dialogueDebut.sentences[0] = "Ouuuuhhhhh ......... J'ai mal à la tête ..... Qu'est ce qu'il m'est arrivé ? Ou est ce que je suis ?";
-        dialogueDebut.sentences[1] = "Je suis sur une île ?! Mais comment c'est possible ? Je ne me souviens de rien.. Ca ne m'a pas l'air très habité..";
-        dialogueDebut.sentences[2] = "Comment je vais partir d'ici ? ... Mmmmmhhhh ... Et si je construisais un radeau !";
-        dialogueDebut.sentences[3] = "Il doit bien y avoir de quoi construire un radeau ici! Commençons par explorer cette plage !";
-        FindObjectOfType<DialogueManager>().StartDialogue(dialogueDebut);
+        dialogue.name = "Judy";
+        dialogue.sentences = new string[4];
+        dialogue.sentences[0] = "Ouuuuhhhhh ......... J'ai mal à la tête ..... Qu'est ce qu'il m'est arrivé ? Ou est ce que je suis ?";
+        dialogue.sentences[1] = "Je suis sur une île ?! Mais comment c'est possible ? Je ne me souviens de rien.. Ca ne m'a pas l'air très habité..";
+        dialogue.sentences[2] = "Comment je vais partir d'ici ? ... Mmmmmhhhh ... Et si je construisais un radeau !";
+        dialogue.sentences[3] = "Il doit bien y avoir de quoi construire un radeau ici! Commençons par explorer cette plage !";
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
     }
 
     public void TriggerDialogueFin()
     {
-        dialogueDebut.name = "Judy";
-        dialogueDebut.sentences = new string[1];
+        dialogue.name = "Judy";
+        dialogue.sentences = new string[1];
  
-        FindObjectOfType<DialogueManager>().StartDialogue(dialogueFin);
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
     }
 
     public void TriggerDialogueTotemOurs()
     {
-        dialogueDebut.name = "Judy";
-        dialogueDebut.sentences = new string[1];
+        dialogue.name = "Judy";
+        dialogue.sentences = new string[1];
 
-        FindObjectOfType<DialogueManager>().StartDialogue(dialogueTotemOurs);
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
     }
 
     public void TriggerDialogueTotemPuma()
     {
-        dialogueDebut.name = "Judy";
-        dialogueDebut.sentences = new string[1];
+        dialogue.name = "Judy";
+        dialogue.sentences = new string[1];
 
-        FindObjectOfType<DialogueManager>().StartDialogue(dialogueTotemPuma);
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
     }
 
     public void TriggerDialogueVoile()
     {
-        dialogueDebut.name = "Judy";
-        dialogueDebut.sentences = new string[6];
+        dialogue.name = "Judy";
+        dialogue.sentences = new string[6];
 
 
-        FindObjectOfType<DialogueManager>().StartDialogue(dialogueVoile);
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
     }
 
     public void TriggerDialogueCorde()
     {
-        dialogueDebut.name = "Judy";
-        dialogueDebut.sentences = new string[6];
+        dialogue.name = "Judy";
+        dialogue.sentences = new string[6];
 
-        FindObjectOfType<DialogueManager>().StartDialogue(dialogueCorde);
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
     }
 
 }
---
+++ Assets/Scripts/Menus.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menus : MonoBehaviour {
+
+    //Lance la scène Map Island et détruit la scène actuelle "Menu_demarrer"
+	public void LancerPartie()
+    {
+        SceneManager.LoadScene("Map Island");
+    }
+
+    //Créer une nouvelle partie et écrase l'ancienne
+    public void NouvellePartie()
+    {
+        
+    }
+
+    //Quitter le jeu
+    public void Quitter()
+    {
+        Application.Quit();
+    }
+
+    //Sauvegarder le jeu
+    public void Sauvegarder()
+    {
+        
+    }
+
+    //Recommencer permet de relancer le jeu avant la mort du personnage
+    public void Recommencer()
+    {
+
+    }
+
+    //Permet de revenir au menu demarrer pour quitter le jeu
+    public void QuitterMapIsland()
+    {
+        SceneManager.LoadScene("Menu_demarrer");
+    }
+}
