e5ad18f7d49c7dcd658a7e67d6f456332e6b6cfc
unknown <noemy.artigouha@gmail.com>
2017-11-20 13:55:01 -0500

    creation de tous les dialogues

--- Assets/Dialogues/DialogueTrigger.cs
+++ Assets/Dialogues/DialogueTrigger.cs
@@ -21,40 +21,53 @@ public class DialogueTrigger : MonoBehaviour {
     {
         dialogue.name = "Judy";
         dialogue.sentences = new string[1];
+        dialogue.sentences[0] = "Ca y est !!!! Je peux enfin quitter cette île !!!!!!!!! Il était temps ...";
  
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
     }
 
     public void TriggerDialogueTotemOurs()
     {
-        dialogue.name = "Judy";
-        dialogue.sentences = new string[1];
-
+        dialogue.name = "Aide";
+        dialogue.sentences = new string[3];
+        dialogue.sentences[0] = "Félicitation! Vous venez de trouver le totem Ours. Cet item vous permets de vous transformer en ours.";
+        dialogue.sentences[1] = "Dans cette forme, vous êtes plus résistante et vous pourrez accéder à de nouvelles énigmes.";
+        dialogue.sentences[2] = "Une roue de transformation est maintenant accéssible si vous maintenez A. Vous pouvez choisir votre forme à l'aide du clic gauche de la souris";
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
     }
 
     public void TriggerDialogueTotemPuma()
     {
         dialogue.name = "Judy";
-        dialogue.sentences = new string[1];
-
+        dialogue.sentences = new string[2];
+        dialogue.sentences[0] = "Félicitation! Vous venez de trouver le totem Puma. Cet item vous permets de vous transformer en puma.";
+        dialogue.sentences[1] = "Dans cette forme, vous êtes moins résistante mais vous pourrez obtenir de meilleurs mouvements et ainsi accéder à de nouvelles énigmes.";
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
     }
 
     public void TriggerDialogueVoile()
     {
         dialogue.name = "Judy";
-        dialogue.sentences = new string[6];
-
-
+        dialogue.sentences = new string[1];
+        dialogue.sentences[0] = "Félicitation! Vous venez de trouver une voile. Cet item vous permettra de construire un radeau.";
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
     }
 
     public void TriggerDialogueCorde()
     {
         dialogue.name = "Judy";
-        dialogue.sentences = new string[6];
+        dialogue.sentences = new string[1];
+        dialogue.sentences[0] = "Félicitation! Vous venez de trouver une corde. Cet item vous permettra de construire un radeau.";
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
 
+    public void TriggerDialogueArc()
+    {
+        dialogue.name = "Judy";
+        dialogue.sentences = new string[6];
+        dialogue.sentences[0] = "Félicitation! Vous venez de trouver un arc. Cet item vous permettra de chasser et de vous défendre.";
+        dialogue.sentences[1] = "Des éléments sur l'île vous permettront de créer des flèches.";
+        dialogue.sentences[2] = "Pour l'utiliser: ";
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
     }
 
