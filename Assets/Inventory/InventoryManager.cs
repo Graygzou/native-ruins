545430b05d6632b43204c1ef0700604e758bf6af
gcichocki <guilhem.cichocki@gmail.com>
2017-11-21 18:46:46 -0500

    Craft et polish de l'inventaire

--- Assets/Inventory/InventoryManager.cs
+++ Assets/Inventory/InventoryManager.cs
@@ -9,9 +9,14 @@ public class InventoryManager : MonoBehaviour {
 	[SerializeField] private RectTransform m_bag;
 	[SerializeField]private RectTransform b_anchor;
 	[SerializeField]private AudioSource m_bagSound;
+	[SerializeField]private AudioSource m_craftSound;
 
 	[SerializeField]private RectTransform o_Bow;
 	[SerializeField]private RectTransform o_Torch;
+	[SerializeField]private RectTransform o_Bonfire;
+	[SerializeField]private RectTransform o_Raft;
+	[SerializeField]private RectTransform o_Plank;
+	[SerializeField]private RectTransform o_Arrow;
 
     private static int nbArrow;
 
@@ -27,6 +32,7 @@ public class InventoryManager : MonoBehaviour {
 	void Start () {
 		deltaScreen = m_canvas.sizeDelta;
         nbArrow = 0;
+		ActiveRedCross();
 
     }
 	
@@ -36,6 +42,7 @@ public class InventoryManager : MonoBehaviour {
 		OpenOrCloseInventory ();
 		PutWeaponInBag ();
         //NumberOfArrow();
+		ActiveRedCross();
     }
 
 	public ArrayList GetInventory(){
@@ -136,6 +143,203 @@ public class InventoryManager : MonoBehaviour {
             GameObject.Find("Affichages/Arrow/Nb_arrow").GetComponent<Text>().text = "x " + nbArrow;
         }
     }
+
+	private void ActiveRedCross(){
+		int wood = 0;
+		int flint = 0;
+		int plank = 0;
+		int sail = 0;
+		int rope = 0;
+
+		foreach (ObjectsType obj in inventaire) {
+			switch (obj) {
+			case ObjectsType.Wood:
+				wood++;
+				break;
+			case ObjectsType.Rope:
+				rope++;
+				break;
+			case ObjectsType.Flint:
+				flint++;
+				break;
+			case ObjectsType.Plank:
+				plank++;
+				break;
+			case ObjectsType.Sail:
+				sail++;
+				break;
+			}
+		}
+		if (wood >= 2) {
+			GameObject.Find ("InventoryManager/Canvas/Craft/Plank/RedCross").SetActive (false);
+			GameObject.Find ("InventoryManager/Canvas/Craft/Plank/Button").SetActive (true);
+		} else {
+			GameObject.Find ("InventoryManager/Canvas/Craft/Plank/RedCross").SetActive (true);
+			GameObject.Find ("InventoryManager/Canvas/Craft/Plank/Button").SetActive (false);
+		}
+		if (wood >= 1 && flint >=1) {
+			GameObject.Find ("InventoryManager/Canvas/Craft/Arrow/RedCross").SetActive (false);
+			GameObject.Find ("InventoryManager/Canvas/Craft/Arrow/Button").SetActive (true);
+			GameObject.Find ("InventoryManager/Canvas/Craft/Torch/RedCross").SetActive (false);
+			GameObject.Find ("InventoryManager/Canvas/Craft/Torch/Button").SetActive (true);
+		} else {
+			GameObject.Find ("InventoryManager/Canvas/Craft/Arrow/RedCross").SetActive (true);
+			GameObject.Find ("InventoryManager/Canvas/Craft/Arrow/Button").SetActive (false);
+			GameObject.Find ("InventoryManager/Canvas/Craft/Torch/RedCross").SetActive (true);
+			GameObject.Find ("InventoryManager/Canvas/Craft/Torch/Button").SetActive (false);
+		}
+		if (wood >= 3 && flint >=1) {
+			GameObject.Find ("InventoryManager/Canvas/Craft/Bonfire/RedCross").SetActive (false);
+			GameObject.Find ("InventoryManager/Canvas/Craft/Bonfire/Button").SetActive (true);
+		} else {
+			GameObject.Find ("InventoryManager/Canvas/Craft/Bonfire/RedCross").SetActive (true);
+			GameObject.Find ("InventoryManager/Canvas/Craft/Bonfire/Button").SetActive (false);
+		}
+		if (plank >= 5 && sail >=1 && rope >=1) {
+			GameObject.Find ("InventoryManager/Canvas/Craft/Raft/RedCross").SetActive (false);
+			GameObject.Find ("InventoryManager/Canvas/Craft/Raft/Button").SetActive (true);
+		} else {
+			GameObject.Find ("InventoryManager/Canvas/Craft/Raft/RedCross").SetActive (true);
+			GameObject.Find ("InventoryManager/Canvas/Craft/Raft/Button").SetActive (false);
+		}
+	}
+
+	public void CraftArrow(){
+		m_craftSound.Play ();
+		int wood = 1;
+		int flint = 1;
+			int i = 0;
+		bool trouve = false;
+		Transform bag = GameObject.Find("Affichages/InventoryManager/Canvas/Bag").transform;
+		while (i < bag.childCount && !trouve) {
+			if((bag.GetChild(i).GetComponent<ObjectScript>().o_type == ObjectsType.Wood) && wood>0) {
+				wood--;
+				Destroy(bag.GetChild(i).gameObject);
+				RemoveObjectOfType (ObjectsType.Wood);
+			}else if((bag.GetChild(i).GetComponent<ObjectScript>().o_type == ObjectsType.Flint) && flint>0) {
+				flint--;
+				Destroy(bag.GetChild(i).gameObject);
+				RemoveObjectOfType (ObjectsType.Flint);
+			}
+			trouve = (flint == 0) && (wood == 0);
+			i++;
+		}
+
+		InventoryManager.AddObjectOfType(ObjectsType.Arrow);
+		InventoryManager.AddObjectOfType(ObjectsType.Arrow);
+		InventoryManager.AddObjectOfType(ObjectsType.Arrow);
+		RectTransform clone1 = Instantiate(o_Arrow) as RectTransform;
+		clone1.SetParent (GameObject.Find("InventoryManager/Canvas/Bag").transform, false);
+		RectTransform clone2 = Instantiate(o_Arrow) as RectTransform;
+		clone2.SetParent (GameObject.Find("InventoryManager/Canvas/Bag").transform, false);
+		RectTransform clone3 = Instantiate(o_Arrow) as RectTransform;
+		clone3.SetParent (GameObject.Find("InventoryManager/Canvas/Bag").transform, false);
+	}
+
+	public void CraftPlank(){
+		m_craftSound.Play ();
+		int wood = 2;
+		int i = 0;
+		bool trouve = false;
+		Transform bag = GameObject.Find("Affichages/InventoryManager/Canvas/Bag").transform;
+		while (i < bag.childCount && !trouve) {
+			if((bag.GetChild(i).GetComponent<ObjectScript>().o_type == ObjectsType.Wood) && wood>0) {
+				wood--;
+				Destroy(bag.GetChild(i).gameObject);
+				RemoveObjectOfType (ObjectsType.Wood);
+			}
+			trouve = (wood == 0);
+			i++;
+		}
+
+
+		InventoryManager.AddObjectOfType(ObjectsType.Plank);
+		RectTransform clone1 = Instantiate(o_Plank) as RectTransform;
+		clone1.SetParent (GameObject.Find("InventoryManager/Canvas/Bag").transform, false);
+	}
+
+	public void CraftTorch(){
+		m_craftSound.Play ();
+		int wood = 1;
+		int flint = 1;
+		int i = 0;
+		bool trouve = false;
+		Transform bag = GameObject.Find("Affichages/InventoryManager/Canvas/Bag").transform;
+		while (i < bag.childCount && !trouve) {
+			if((bag.GetChild(i).GetComponent<ObjectScript>().o_type == ObjectsType.Wood) && wood>0) {
+				wood--;
+				Destroy(bag.GetChild(i).gameObject);
+				RemoveObjectOfType (ObjectsType.Wood);
+			}else if((bag.GetChild(i).GetComponent<ObjectScript>().o_type == ObjectsType.Flint) && flint>0) {
+				flint--;
+				Destroy(bag.GetChild(i).gameObject);
+				RemoveObjectOfType (ObjectsType.Flint);
+			}
+			trouve = (flint == 0) && (wood == 0);
+			i++;
+		}
+
+		InventoryManager.AddObjectOfType(ObjectsType.Torch);
+		RectTransform clone1 = Instantiate(o_Torch) as RectTransform;
+		clone1.SetParent (GameObject.Find("InventoryManager/Canvas/Bag").transform, false);
+	}
+
+	public void CraftBonfire(){
+		m_craftSound.Play ();
+		int wood = 3;
+		int flint = 1;
+		int i = 0;
+		bool trouve = false;
+		Transform bag = GameObject.Find("Affichages/InventoryManager/Canvas/Bag").transform;
+		while (i < bag.childCount && !trouve) {
+			if((bag.GetChild(i).GetComponent<ObjectScript>().o_type == ObjectsType.Wood) && wood>0) {
+				wood--;
+				Destroy(bag.GetChild(i).gameObject);
+				RemoveObjectOfType (ObjectsType.Wood);
+			}else if((bag.GetChild(i).GetComponent<ObjectScript>().o_type == ObjectsType.Flint) && flint>0) {
+				flint--;
+				Destroy(bag.GetChild(i).gameObject);
+				RemoveObjectOfType (ObjectsType.Flint);
+			}
+			trouve = (flint == 0) && (wood == 0);
+			i++;
+		}
+
+		InventoryManager.AddObjectOfType(ObjectsType.Fire);
+		RectTransform clone1 = Instantiate(o_Bonfire) as RectTransform;
+		clone1.SetParent (GameObject.Find("InventoryManager/Canvas/Bag").transform, false);
+	}
+
+	public void CraftRaft(){
+		m_craftSound.Play ();
+		int rope = 1;
+		int sail = 1;
+		int plank = 5;
+		int i = 0;
+		bool trouve = false;
+		Transform bag = GameObject.Find("Affichages/InventoryManager/Canvas/Bag").transform;
+		while (i < bag.childCount && !trouve) {
+			if((bag.GetChild(i).GetComponent<ObjectScript>().o_type == ObjectsType.Plank) && plank>0) {
+				plank--;
+				Destroy(bag.GetChild(i).gameObject);
+				RemoveObjectOfType (ObjectsType.Plank);
+			}else if((bag.GetChild(i).GetComponent<ObjectScript>().o_type == ObjectsType.Sail) && sail>0) {
+				sail--;
+				Destroy(bag.GetChild(i).gameObject);
+				RemoveObjectOfType (ObjectsType.Sail);
+			}else if((bag.GetChild(i).GetComponent<ObjectScript>().o_type == ObjectsType.Rope) && rope>0) {
+				rope--;
+				Destroy(bag.GetChild(i).gameObject);
+				RemoveObjectOfType (ObjectsType.Rope);
+			}
+			trouve = (sail == 0) && (plank == 0) && (rope == 0);
+			i++;
+		}
+
+		InventoryManager.AddObjectOfType(ObjectsType.Raft);
+		RectTransform clone1 = Instantiate(o_Raft) as RectTransform;
+		clone1.SetParent (GameObject.Find("InventoryManager/Canvas/Bag").transform, false);
+	}
 
     //private void NumberOfArrow() {
     //    int nbArrow = 0;
