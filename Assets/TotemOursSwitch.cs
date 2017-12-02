c0cf5dea0907febfca0b8e54bb4138919bcc2baa
Damien Maveyraud <damienmaveyraud@hotmail.fr>
2017-12-02 00:09:27 -0500

    Polishing : Cutscene approche totem ours

---
+++ Assets/TotemOursSwitch.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TotemOursSwitch : Switch {
+
+    private bool PlayedOnce;
+
+    // Use this for initialization
+    void Start()
+    {
+        PlayedOnce = false;
+        if (cameraCutScene != null)
+            cameraCutScene.enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other == GameObject.FindWithTag("Player").GetComponent<Collider>() && (PlayedOnce == false))
+        {
+            PlayedOnce = true;
+            Activate();
+        }
+    }
+
+    override public IEnumerator PlayCutScene()
+    {
+        cameraCutScene.GetComponent<Animator>().Play("CutsceneOurs");
+        cameraCutScene.GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(10f);
+        //ActivateSwitch();
+        //yield return new WaitForSeconds(1f);
+        StopCutScene();
+        if (gameObject.GetComponent<SwitchObject>() != null)
+        {
+            // Activate all his children
+            gameObject.GetComponent<SwitchObject>().ActivateChildren();
+        }
+    }
+
+    // The switch does what he's meant for here.
+    override protected void ActivateSwitch()
+    {
+        GameObject judy = GameObject.FindWithTag("Player");
+
+    }
+
+    override protected void DiactivateSwitch()
+    {
+        GameObject judy = GameObject.FindWithTag("Player");
+    }
+}
