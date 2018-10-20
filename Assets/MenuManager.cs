b0859f6db0113052873317698455ce21757d75fe
Graygzou <gregoire.boiron@gmail.com>
2018-10-19 21:16:39 -0400

    Merged the two menu scripts together.

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour {
 
@@ -22,10 +24,16 @@ public class MenuManager : MonoBehaviour {
 
     #region Serialize fields
     [SerializeField]
-    private GameObject pause;
+    private string mainSceneName = "Map Island";
 
     [SerializeField]
-    private GameObject help;
+    private Button loadButton;
+
+    [SerializeField]
+    private GameObject pause = null;
+
+    [SerializeField]
+    private GameObject help = null;
 
     private Animator animator;
     #endregion
@@ -43,7 +51,6 @@ public class MenuManager : MonoBehaviour {
         _instance.animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -51,6 +58,17 @@ public class MenuManager : MonoBehaviour {
             pause.SetActive(!pause.activeSelf);
         }
 
+    }
+
+    private void Start()
+    {
+        PlayerPrefs.SetInt("load_scene", 0);
+
+        // Enable or not the launch game button
+        if (PlayerPrefs.HasKey("xPlayer"))
+        {
+            loadButton.enabled = true;
+        }
     }
 
     public void DisplayHelpMenu()
@@ -61,6 +79,37 @@ public class MenuManager : MonoBehaviour {
     public void CloseHelpMenu()
     {
         _instance.help.SetActive(false);
+    }
+
+    //Lance la scène Map Island et détruit la scène actuelle "Menu_demarrer"
+    public void LancerPartie()
+    {
+        //Permet de verifier qu'il y ait bien une sauvegarde
+        if(PlayerPrefs.HasKey("xPlayer"))
+        {
+            PlayerPrefs.SetInt("load_scene", 1);
+            //Chargement de la scene
+            SceneManager.LoadScene(PlayerPrefs.GetString("scene"));
+        }
+        else
+        {
+            Debug.LogWarning("The player should have saved data to be able to click on this button..");
+        }
+    }
+
+    //Créer une nouvelle partie et écrase l'ancienne
+    public void NouvellePartie()
+    {
+        SceneManager.LoadScene(mainSceneName);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit ();
+#endif
     }
 
     public void TransitionToNextPanel(MenuPanel nextPanel)
